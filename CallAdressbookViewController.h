eab9f262a45370e5faed49abb0d0ba176fab74d4
xitaoque <quexitao@outlook.com>
2016-02-04 23:07:50 +0800

    Create CallAdressbookViewController.h

---
+++ CallAdressbookViewController.h
@@ -0,0 +1,10 @@
+
+#import <UIKit/UIKit.h>
+
+
+@interface CallAdressbookViewController : UIViewController
+
+@property (nonatomic, strong)NSString *contactName;
+@property (nonatomic, strong)NSString *contactPhoneNumber;
+
+@end
